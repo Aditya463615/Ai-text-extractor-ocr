98d86fcdcf0bd3d8d54eafddeecc921d7f33913e
ad_nobita <87667895+Aditya463615@users.noreply.github.com>
2024-03-23 00:17:26 +0530

    Create ocr.py

---
+++ ocr.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import sys
+
+parser_id = "visit website"
+apikey = 'visit website
+
+url = f"https://api.documentpro.ai/files/upload/{parser_id}"
+payload = {}
+files=[
+    ('file',('aadhar.jpeg', open(input('File: '),'rb'),'application/jpg'))
+]
+headers = {
+    'x-api-key': apikey
+}
+
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+request_id = json.loads(response.content)['request_id']
+
+url = f"https://api.documentpro.ai/files?request_id={request_id}"
+
+payload = {}
+headers = {
+    'x-api-key': apikey
+}
+
+response = json.loads(requests.request("GET", url, headers=headers, data=payload).text)
+while response['response_body']["result_json_data"] == None:
+    
+    url = f"https://api.documentpro.ai/files?request_id={request_id}"
+
+    payload = {}
+    headers = {
+        'x-api-key': apikey
+    }
+
+    response = json.loads(requests.request("GET", url, headers=headers, data=payload).text)
+
+print(response['response_body']["result_json_data"])
+with open('response.ocr','a') as file:
+    file.write(str(response['response_body']["result_json_data"]))
+
+input('time to exit')
